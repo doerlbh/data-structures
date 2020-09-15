7f5bfc8c3695cae069734650a6e31070e3856d25
Ubuntu <ubuntu@ip-172-31-31-148.us-east-2.compute.internal>
2020-09-15 18:58:39 +0000

    [FEAT] update w2

--- week02/README.md
+++ week02/README.md
@@ -21,13 +21,12 @@ AWS Cloud9, Node.js, cheerio, basic javascript
 ## Key solution
 
 ```javascript
-var addresses = $('.detailsBox')                                   // locate detailsBox
-                    .map( (i, elem) => $(elem.parentNode).text()   // get parent node
-                                            .split("\n")[3].trim() // get 3rd row (with address)
-                                            .split(',')[0].trim()  // get only street (no room)
-                                            .split('-')[0])        // remove extra info (no floor)
-                    .filter( (v, i, a) => a.indexOf(v) === i )     // remove duplicate
-                    .get(); 
+var addresses = $('.detailsBox')                                    // locate detailsBox
+                    .map( (i,elem) => $(elem.parentNode).text()     // get parent node
+                                            .split("\n")[3].trim()  // get 3rd row (with address)
+                                            .split(',')[0].trim()   // get only street (no room)
+                                            .split('-')[0].trim() ) // remove extra info (no floor)
+                    .get();
 ```
 
 
--- week02/week02.js
+++ week02/week02.js
@@ -6,12 +6,12 @@ var cheerio = require('cheerio');
 var content = fs.readFileSync('data/m07.txt');
 var $ = cheerio.load(content);
 
-var addresses = $('.detailsBox')                                   // locate detailsBox
-                    .map( (i, elem) => $(elem.parentNode).text()   // get parent node
-                                            .split("\n")[3].trim() // get 3rd row (with address)
-                                            .split(',')[0].trim()  // get only street (no room)
-                                            .split('-')[0])        // remove extra info (no floor)
-                    .filter( (v, i, a) => a.indexOf(v) === i )     // remove duplicate
-                    .get(); 
-                    
-fs.writeFileSync('data/m07addresses.txt', addresses.unique().join('\n'));
+var addresses = $('.detailsBox')                                    // locate detailsBox
+                    .map( (i,elem) => $(elem.parentNode).text()     // get parent node
+                                            .split("\n")[3].trim()  // get 3rd row (with address)
+                                            .split(',')[0].trim()   // get only street (no room)
+                                            .split('-')[0].trim() ) // remove extra info (no floor)
+                    .get();
+
+fs.writeFileSync('m07_addresses.txt', addresses.join('\n'));
+
