e22b3d8ba447f06305a7aa3094e90a7fa5abae66
Ubuntu <ubuntu@ip-172-31-31-148.us-east-2.compute.internal>
2020-10-19 22:33:26 +0000

    [FEAT] finish extract meeting info

--- week07/crawlData.js
+++ week07/crawlData.js
@@ -1,70 +1,69 @@
-// -address
-//     -geolocation
-// -day
-// -time
-// -type
-// -wheelchair
-// -(special interest)
-// -(notes)
-// -(location notes? I.e. room number, floor number)
-// -(building name)
-// -(title)
-
-
 // dependencies
 const fs = require('fs'),
-      querystring = require('querystring'),
-      request = require('request'),
-      async = require('async'),
-      dotenv = require('dotenv');
+    querystring = require('querystring'),
+    request = require('request'),
+    async = require('async'),
+        dotenv = require('dotenv');
 
 var cheerio = require('cheerio');
 
+for (let i = 1; i < 11; i++) {
 
-var i = 7;
-var filecount = ('0' + i).slice(-2);
+    let filecount = ('0' + i).slice(-2);
 
-var content = fs.readFileSync('../data/html/m'+filecount+'.txt');
-var $ = cheerio.load(content);
+    var content = fs.readFileSync('../data/html/m' + filecount + '.txt');
+    var $ = cheerio.load(content);
 
-var addresses = $('h4').slice(2)                                    // locate h4
-                    .map( (i,elem) => $(elem.parentNode).text()     // get parent node
-                                            .split("\n")[3].trim()  // get 3rd row (with address)
-                                            .split(',')[0].trim()   // get only street (no room)
-                                            .split('-')[0].trim() ) // remove extra info (no floor)
-                    .get();
+    var addresses = $('h4').slice(2) // extract address name 
+        .map((i, elem) => $(elem.parentNode).text()
+            .split("\n")[3].trim()
+            .split(',')[0].trim()
+            .split('-')[0].trim())
+        .get();
 
-fs.writeFileSync('../data/address/m'+filecount+'_addresses.json', JSON.stringify(addresses));
+    fs.writeFileSync('../data/address/m' + filecount + '_addresses.json', JSON.stringify(addresses));
 
-var meetinginfo = $('td[style="border-bottom:1px solid #e3e3e3;width:350px;"]') // extract all meeting info
-                    .map( (i,elem) => $(elem).text() 
-                                        .split("\n")
-                                        .map( x => x.trim() )
-                                        .filter( x => x.length > 0 )
-                                        .join('\n') )
-                    .get();
+    var meetinginfo = $('td[style="border-bottom:1px solid #e3e3e3;width:350px;"]') // extract full meeting info
+        .map((i, elem) => $(elem).text()
+            .split("\n")
+            .map(x => x.trim())
+            .filter(x => x.length > 0)
+            .join('\n'))
+        .get();
 
-var addressinfo = $('h4').slice(2)                                   
-                    .map( (i,elem) => $(elem.parentNode).html()    
-                                            .split("\n")
-                                            .map( x => x.trim() )
-                                            .filter( x => x.length > 0 )
-                                            .join('\n')) 
-                    .get();
+    var addressinfo = $('h4').slice(2) // extract full address info     
+        .map((i, elem) => $(elem.parentNode).html()
+            .split("\n")
+            .map(x => x.trim())
+            .filter(x => x.length > 0)
+            .join('\n')
+            .replace('<br>', ''))
+        .get();
 
-var meetings = [];
-meetinginfo.forEach( (i,x) => {
-    var submeetings = {
-        
-    };
-    submeetings.forEach( (j,meeting) => {
-        meetings.push(meeting);
+    var meetings = [];
+    meetinginfo.forEach((meetingset, i) => {
+        var submeetings = meetingset.split('\n').map((mtext) => {
+            var meeting = {};
+            meeting.address = addresses[i];
+            meeting.building = addressinfo[i].split('</h4>')[0].split('>')[1].replace('<br>', '').trim();
+            meeting.title = (addressinfo[i].split('\n')[1].startsWith('<b>') ? addressinfo[i].split('\n')[1].split('</b>')[0].split('<b>')[1].trim() : '');
+            meeting.address_notes = addressinfo[i].split('\n').slice(2, 4).join('').replace(',', ', ').replace('  ', ' ').replace('<br>', '').trim();
+            meeting.meeting_notes = (addressinfo[i].includes('detailsBox') ? addressinfo[i].split('"detailsBox">')[1].split('</div')[0].replace('\n', '').replace('<br>', '').trim() : '');
+            meeting.zipcode = meeting.address_notes.slice(meeting.address_notes.length - 5);
+            meeting.wheelchair = (addressinfo[i].includes('wheelchair') ? true : false);
+            meeting.day = mtext.split(' ')[0].trim();
+            meeting.start = mtext.split('From')[1].split('to')[0].trim();
+            meeting.end = mtext.split('to')[1].trim().split(' ').slice(0, 2).join(' ').trim();
+            meeting.type = (mtext.includes('Type') ? mtext.split('Type')[1].trim().split('Special')[0].trim() : '');
+            meeting.special = (mtext.includes('Special Interest') ? mtext.split('Special Interest')[1].trim() : '');
+            return meeting; 
+        });
+
+        submeetings.forEach((meeting) => {
+            meetings.push(meeting);
+        });
     });
-}); 
-                    
-console.log(addressinfo[47]);
 
+    fs.writeFileSync('../data/meeting/m' + filecount + '_meetings.json', JSON.stringify(meetings));
 
-// WORK IN PROGRESS FOR THIS PART: for final visualization exercise.
-// console.log(mtgday.length);
-// fs.writeFileSync('m07_data.json', JSON.stringify(data));
+}
